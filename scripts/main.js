cb15dec9be2985d55ed5a27200a847225418d134
caoying <leizhixing@qq.com>
2020-07-16 23:11:11 +0800

    1

---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+$(".show-mask").on("click",function(){$(".serve-channel").css("display","flex")}),$(".serve-channel").on("click",function(){$(".serve-channel").css("display","none")});
+//# sourceMappingURL=main.js.map
